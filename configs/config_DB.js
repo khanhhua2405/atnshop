356f40525e658917fde987959f737883f1b067dd
khanhhua2405 <71120689+khanhhua2405@users.noreply.github.com>
2020-12-24 10:44:40 +0700

    Add files via upload

---
+++ configs/config_DB.js
@@ -0,0 +1,18 @@
+var DBconfigs = {
+    localdb: {
+        urldb : 'mongodb+srv://Proplayer:ZseZxcJUBjGtLQ0y@cluster0.9yrzf.mongodb.net/<ExampleDB>?retryWrites=true&w=majority',
+        dbname : "ExampleDB",
+        dbusername : "",
+        dbpassword : "",
+    },
+
+    clouddb: {
+        urldb : "mongodb+srv://Toan123:Toan123@cluster0.9yrzf.mongodb.net/ExampleDB?retryWrites=true&w=majority",
+        dbname : "ExampleDB",
+        dbusername : "",
+        dbpassword : "",
+    }
+
+};
+
+module.exports = DBconfigs.clouddb;
