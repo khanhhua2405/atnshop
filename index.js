a07f919491cab81b5df987c5f68f7c543835bb02
khanhhua2405 <71120689+khanhhua2405@users.noreply.github.com>
2020-12-24 11:56:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.set('view engine', 'ejs');
 /// 					 		Config
 /// ................................................................
 /// Tham số
-const PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 app.listen(PORT)
 
 /// ------------------ Khai bao cac Folder Tĩnh, Session, Cookies
